15d01756cc53e94b60f6507a138a082f3fa1f091
Albert Mithamo <albertmwendia47@gmail.com>
2025-06-27 11:08:26 +0300

    bookstore schema

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -36,7 +36,8 @@ CREATE TABLE Orders(
 order_id INT PRIMARY KEY,
 customer_id INT,
 order_date DATE,
-FOREIGN KEY (customer_id) REFERENCES Customers (customer_id)
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+
 
 );
 
@@ -45,8 +46,8 @@ orderdetailid INT PRIMARY KEY,
 order_id INT,
 book_id INT,
 quantity DOUBLE,
-FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-FOREIGN KEY (book_id) REFERENCES Books (book_id)
+FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+FOREIGN KEY (book_id) REFERENCES Books(book_id)
 
 );
 
